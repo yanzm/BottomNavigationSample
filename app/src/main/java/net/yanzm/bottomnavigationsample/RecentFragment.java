3e8fae2580e1ace630716b39109be43cba3cecca
Yuki Anzai <anzai.y.aa@gmail.com>
2017-04-13 15:01:12 +0900

    delete logging

--- app/src/main/java/net/yanzm/bottomnavigationsample/RecentFragment.java
+++ app/src/main/java/net/yanzm/bottomnavigationsample/RecentFragment.java
@@ -3,7 +3,6 @@ package net.yanzm.bottomnavigationsample;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,17 +13,5 @@ public class RecentFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_recent, container, false);
-    }
-
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        Log.d("ここ", "onCreate");
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        Log.d("ここ", "onDestroy");
     }
 }
